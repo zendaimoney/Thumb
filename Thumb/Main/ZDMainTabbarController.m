5ef029f2c1a71bccd2e1f014c1f3222616296f15
peter.shi <ios@zhangyuqing.local>
2014-11-12 11:56:35 +0800

    add pictures

--- Thumb/Main/ZDMainTabbarController.m
+++ Thumb/Main/ZDMainTabbarController.m
@@ -28,6 +28,29 @@
     UINavigationController *nav3 = [storyboard3 instantiateViewControllerWithIdentifier:@"storyBoardThreeNav"];
     
     self.viewControllers = @[nav1,nav2,nav3];
+    
+    for (int i = 0; i < self.tabBar.items.count; i++) {
+        UITabBarItem *barItem = self.tabBar.items[i];
+        switch (i) {
+            case 0:
+                barItem.title = @"理财规划";
+                barItem.image = [UIImage imageNamed:@"ico_bottom_fortune"];
+                barItem.selectedImage = [UIImage imageNamed:@"ico_bottom_fortune_on"];
+                break;
+            case 1:
+                barItem.title = @"我的账户";
+                barItem.image = [UIImage imageNamed:@"ico_bottom_account"];
+                barItem.selectedImage = [UIImage imageNamed:@"ico_bottom_account_on"];
+                break;
+            case 2:
+                barItem.title = @"关于我们";
+                barItem.image = [UIImage imageNamed:@"icon_bottom_about"];
+                barItem.selectedImage = [UIImage imageNamed:@"icon_bottom_about_on"];
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 - (void)viewDidLoad {
