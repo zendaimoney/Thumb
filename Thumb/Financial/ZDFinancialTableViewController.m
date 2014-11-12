40499b6c55273ce36bc262c3dc858b38c834a065
peter.shi <ios@zhangyuqing.local>
2014-11-12 15:22:10 +0800

    ssf

--- Thumb/Financial/ZDFinancialTableViewController.m
+++ Thumb/Financial/ZDFinancialTableViewController.m
@@ -8,8 +8,9 @@
 
 #import "ZDFinancialTableViewController.h"
 #import "ZDWebService.h"
+#import "AllCustomerCategoryHeaders.h"
 
-@interface ZDFinancialTableViewController ()
+@interface ZDFinancialTableViewController ()<SSFSegmentControlDelegate>
 
 @end
 
@@ -17,9 +18,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self configureView];
 }
 
+- (void)configureView
+{
+    SSFSegmentControl *segment = [SSFSegmentControl SSFSegmentedControlInstance];
+    segment.delegate = self;
+    self.tableView.tableHeaderView = segment;
+}
+
+#pragma mark - SSFSegmentDelegate
+
+- (void)SSFSegmentControlDidPressed:(UIView *)view selectedIndex:(NSInteger)selectedIndex
+{
+    switch (selectedIndex) {
+        case 0:
+            
+            break;
+        case 1:
+            
+            break;
+        default:
+            break;
+    }
+}
 
 - (IBAction)test:(id)sender {
     [[ZDWebService sharedWebService] loginWithUserName:@"13174125879" password:@"123456" completion:^(NSError *error, NSDictionary *resultDic) {
